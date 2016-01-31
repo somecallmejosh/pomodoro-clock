95db3fdebdd82c770eb9c6637eee16584f870e9b
Joshua Briley <josh@thebrileys.com>
2016-01-31 14:11:55 -0500

    Style and js updates.

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+(function(){
+  // set variables
+  var $breakTimer = $('.set-timer--break'),
+      $sessionTimer = $('.set-timer--session'),
+      $timeIncrease = $('.set-timer--trigger__up'),
+      $timeDecrease = $('.set-timer--trigger__down')
+      $breakIncrease = $breakTimer.find($timeIncrease),
+      $breakDecrease = $breakTimer.find($timeDecrease),
+      $sessionIncrease = $sessionTimer.find($timeIncrease),
+      $sessionDecrease = $sessionTimer.find($timeDecrease);
+
+  // set duration (reuse for break and session)
+    // increase duration
+    // decrease duration
+
+  // Display updated times
+    // break duration
+    // session duration
+
+  // Countdown timer
+    // start timer
+
+    // stop timer
+
+    // toggle between break and session
+
+    // display active timer in view
+
+  // reset timer
+
+
+})();
+
+var $breakTimer = $('.set-timer--break'),
+      $sessionTimer = $('.set-timer--session'),
+      $timeIncrease = $('.set-timer--trigger__up'),
+      $timeDecrease = $('.set-timer--trigger__down')
+      $breakIncrease = $breakTimer.find($timeIncrease),
+      $breakDecrease = $breakTimer.find($timeDecrease),
+      $sessionIncrease = $sessionTimer.find($timeIncrease),
+      $sessionDecrease = $sessionTimer.find($timeDecrease);
