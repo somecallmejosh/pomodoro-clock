02f6747c5d231007009d88b54e2f3a7892c6e4d5
Joshua Briley <josh@thebrileys.com>
2016-01-31 17:03:55 -0500

    Update click events to set timer variables.

--- scripts/main.js
+++ scripts/main.js
@@ -2,40 +2,80 @@
   // set variables
   var $breakTimer = $('.set-timer--break'),
       $sessionTimer = $('.set-timer--session'),
+      $timerLabel = $('.set-timer--label'),
       $timeIncrease = $('.set-timer--trigger__up'),
-      $timeDecrease = $('.set-timer--trigger__down')
+      $timeDecrease = $('.set-timer--trigger__down'),
+      $pomodoroClock = $('.pomodoro--countdown'),
       $breakIncrease = $breakTimer.find($timeIncrease),
       $breakDecrease = $breakTimer.find($timeDecrease),
+      $breakTimerLabel = $breakTimer.find($timerLabel),
       $sessionIncrease = $sessionTimer.find($timeIncrease),
-      $sessionDecrease = $sessionTimer.find($timeDecrease);
+      $sessionDecrease = $sessionTimer.find($timeDecrease),
+      $sessionTimerLabel = $sessionTimer.find($timerLabel);
 
-  // set duration (reuse for break and session)
-    // increase duration
-    // decrease duration
+  var breakTimeDefault = 5,
+      sessionTimeDefault = 25,
+      breakTime = breakTimeDefault,
+      sessionTime = sessionTimeDefault;
 
-  // Display updated times
-    // break duration
-    // session duration
+  function init(){
+    breakTime = breakTimeDefault;
+    sessionTime = sessionTimeDefault;
+    setBreakLabel();
+    setSessionLabel();
+    setPomodoroTimerLabel();
+  }
+  init();
 
-  // Countdown timer
-    // start timer
+  // Set All labels
+  function setBreakLabel(){
+    $breakTimerLabel.html(breakTime);
+  }
+  function setSessionLabel(){
+    $sessionTimerLabel.html(sessionTime);
+  }
+  function setPomodoroTimerLabel(){
+    $pomodoroClock.html(sessionTime);
+  }
+  function increaseBreakTime() {
+    breakTime += 1;
+    setBreakLabel();
+  }
 
-    // stop timer
+  // Adjust Times
+  // Refactor to combine increase and decrease into to functions, as opposed to four
+  var decreaseBreakTime = function() {
+    if(breakTime <= 1) {
+      breakTime;
+    } else {
+      breakTime -= 1;
+    }
+    setBreakLabel();
+  }
 
-    // toggle between break and session
+  function increaseSessionTime() {
+    sessionTime += 1;
+    setSessionLabel();
+    setPomodoroTimerLabel();
+  }
 
-    // display active timer in view
+  var decreaseSessionTime = function() {
+    if(sessionTime <= 1) {
+      sessionTime;
+    } else {
+      sessionTime -= 1;
+    }
+    setSessionLabel();
+    setPomodoroTimerLabel();
+  }
 
-  // reset timer
+  // Click events
+  $breakIncrease.click(increaseBreakTime);
+  $breakDecrease.click(decreaseBreakTime);
+  $sessionIncrease.click(increaseSessionTime);
+  $sessionDecrease.click(decreaseSessionTime);
+  $('.reset').click(init);
 
 
 })();
 
-var $breakTimer = $('.set-timer--break'),
-      $sessionTimer = $('.set-timer--session'),
-      $timeIncrease = $('.set-timer--trigger__up'),
-      $timeDecrease = $('.set-timer--trigger__down')
-      $breakIncrease = $breakTimer.find($timeIncrease),
-      $breakDecrease = $breakTimer.find($timeDecrease),
-      $sessionIncrease = $sessionTimer.find($timeIncrease),
-      $sessionDecrease = $sessionTimer.find($timeDecrease);
